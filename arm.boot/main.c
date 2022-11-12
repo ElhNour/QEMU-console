662a630663f436884fb86af6d2ca79118f050c41
NourElh <hn_el_hassane@esi.dz>
2022-11-12 12:26:44 +0100

    Finished step 2 of week1

--- arm.boot/main.c
+++ arm.boot/main.c
@@ -11,7 +11,7 @@ void _start() {// including both interrupts and traps.
   int i = 0;
   int count = 0;
   uart_send_string(UART0, "\nQuit with \"C-a c\" and then type in \"quit\".\n");
-  uart_send_string(UART0, "\nHello world!\n");
+  uart_send_string(UART1, "\nHello world!\n");
 
   while (1) {
     unsigned char c;
@@ -22,12 +22,12 @@ void _start() {// including both interrupts and traps.
       // this annoying code ;-)
       count++;
       if (count > 10000000) {
-        uart_send_string(UART0, "\n\rZzzz....\n\r");
+        //uart_send_string(UART0, "\n\rZzzz....\n\r");
         count = 0;
       }
     }
     if (c == '\r')
-      uart_send(UART0, '\n');
-    uart_send(UART0, c);
+      uart_send(UART1, '\n');
+    uart_send(UART1, c);
   }
 }
